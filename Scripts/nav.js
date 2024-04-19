3ffba23c1d619484e4c9c133187e02c9aec4c711
theo-martin <theomartin1999@gmail.com>
2024-04-19 14:12:04 +0200

    finition du header

--- Scripts/nav.js
+++ Scripts/nav.js
@@ -1,10 +1,11 @@
-const sidenav = document.querySelector("nav__sidenav");
-const openNav = document.querySelector("nav__burger");
-const closeNav = document.querySelector("nav__sidenav__close");
+const sidenav = document.querySelector(".nav__sidenav"); // Utiliser un seul underscore ici
+const openNav = document.querySelector(".nav__burger");
+const closeNav = document.querySelector(".nav__sidenav__close");
 
 openNav.addEventListener("click", () => {
   sidenav.classList.add("nav__sidenav--active");
 });
+
 closeNav.addEventListener("click", () => {
   sidenav.classList.remove("nav__sidenav--active");
 });
